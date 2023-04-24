d41035cb296e99b1c8e6ad930ef7ce47e6a19f8c
hackersifu <jwmckiddy31@gmail.com>
2023-04-24 10:18:42 -0700

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# log_analysis_buddy
-AI Powered Log Analysis Helper
+# Log Analysis Buddy - AI Powered Log Analysis Helper
+The Log Analysis Buddy is an AI powered log analysis helper that can be used to help analysts perform log analysis. The Log analysis Buddy uses OpenAI to receive log input (in .csv, or comma delimited, format) from an incident responder, to perform additional analysis for the responder. This analysis can be used to augment the responder's investigation, to provide thourogh results for reporting.
+
+## Prerequisites
+The following are prerequisites for using the Log Analysis Buddy:
+- An OpenAPI key (from creating an OpenAI account)
+- Log files in .csv format (comma delimited), placed in the same location as the log_analysis_buddy.py file
+- Python 3.7 or higher
+- OpenAI Python library (installed via pip)
+
+## How to Use
+Steps to use the Log Analysis Buddy:
+1. Install the OpenAI Python library using pip (if not installed already).
+'''
+pip install openai
+OR
+pip3 install openai
+'''
+2. Place the log_analysis_buddy.py file in the same location as the log files to be analyzed.
+3. Run the log_analysis_buddy.py file using Python 3.7 or higher.
+'''
+python log_analysis_buddy.py
+OR
+python3 log_analysis_buddy.py
+'''
+4. Enter the OpenAI API key when prompted.
+5. Enter the name of the log file to be analyzed (including the .csv extension).
+6. Enter context for the analysis (i.e. "Look for malicious trends.").
+
+## Example Output
+'''
+Possible malicious trends:
+-A large number of events coming from a single IP address, particularly one that is not within the AWS infrastructure. This could indicate someone attempting to access AWS resources maliciously.
+'''
+
+## Contributions & Feedback
+For any contributions, feel free to create a [GitHub Pull Request](https://github.com/hackersifu/log_analysis_buddy/pulls). Additionally, you can use the Issues section to report bugs or submit feedback.
+
+## License
+This project is licensed under the Apache 2.0 licensing terms.
---
+++ log_analysis_buddy.py
@@ -0,0 +1,96 @@
+# Log Analysis Buddy - AI Powered Log Analysis Helper
+
+import openai
+import os
+import csv
+import logging
+
+
+analysis_file_var = 'random.txt'
+analysis_file_location = os.getcwd()
+analysis_file_full_path = os.path.join(analysis_file_location, analysis_file_var)
+
+def banner():
+    """Function for Log Analysis Buddy banner"""
+    print('''
+____________________                        
+    _                                       
+    /                                       
+---/-------__----__-                        
+  /      /   ) /   )                        
+_/____/_(___/_(___/_                        
+                 /                          
+             (_ /                           
+____________________________________________
+    __                                      
+    / |                /              ,     
+---/__|----__----__---/---------__-------__-
+  /   |  /   ) /   ) /   /   / (_ ` /   (_ `
+_/____|_/___/_(___(_/___(___/_(__)_/___(__)_
+                           /                
+                       (_ /                 
+_______________________________________     
+    ____                                    
+    /   )               /       /           
+---/__ /------------__-/----__-/-------     
+  /    )   /   /  /   /   /   /   /   /     
+_/____/___(___(__(___/___(___/___(___/_     
+                                    /       
+                                (_ /      
+        Joshua "DozerCat" McKiddy
+        Twitter - @dozercat31
+''')
+    
+def log_analysis_buddy():
+    """Function to run Log Analysis Buddy"""
+    banner()
+    print("Please enter the OpenAI API Key you would like to use for this session.")
+    openai.api_key = input()
+    print("Please provide the log file you would like to analyze. Please ensure that they are in CSV format.")
+    log_file = input()
+    log_location = os.getcwd()
+    log_file_location = os.path.join(log_location, log_file)
+    print("Please provide any additional context you'd like to provide to the Log Analysis Buddy.")
+    additional_context_raw = input()
+    additional_context = str(additional_context_raw)
+
+
+    try:
+        with open(log_file_location, 'r') as csv_file:
+            log_csv_reader = csv.reader(csv_file)
+            with open(analysis_file_var, 'a') as analysis_file:
+                for line in log_csv_reader:
+                    analysis_file.write(str(line))
+                    analysis_file.write("\n")
+
+        with open(analysis_file_var, 'r') as analysis_file:
+            contents = analysis_file.read()
+
+
+        response = openai.Completion.create(
+            engine="text-davinci-002",
+            prompt="""
+            Perform a detailed security analysis of these logs: 
+            """
+            + contents +
+            """
+            Use this additional context when performing the analysis: 
+            """
+            + additional_context,
+            temperature=0.9,
+            max_tokens=2000,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0.6,
+            stop=None
+        )
+
+        print(response["choices"][0]["text"])
+
+        os.remove(analysis_file_var)
+    except Exception as exception_handle:
+        logging.error(exception_handle)
+
+
+if __name__ == '__main__':
+    log_analysis_buddy()
